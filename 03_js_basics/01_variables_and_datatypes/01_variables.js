8187bf3a54c5d071d968c93d399c6aee1aeda341
deependra-parmar <dparmar@qodeleaf.com>
2025-02-07 14:31:54 +0530

    Variables and datatypes

---
+++ 03_js_basics/01_variables_and_datatypes/01_variables.js
@@ -0,0 +1,28 @@
+/* What is JavaScript? ----> cross-platform, object oriented language for interactive web-pages and server-side versions of JS like Node.js
+
+1. Client-Side JS is used in controlling browser and its DOM
+2. Server-Side JS extends its functionality by running JS on the server.
+
+JS is case-sensitive in nature and uses UNICODE CHARACTER SET for encoding.
+Instructions are seperated with ";". Not necessary in case of different instruction in different lines but compulsory when multiple instructions are in the same line
+
+*/
+
+// Variables in JS: JS has three ways of variable declarations
+
+// VAR: for functional or global scoped variables.
+// LET: blocked scope, local variable
+// CONST: blocked scope, read-only named constant
+
+var x = 1;
+let a = 1;
+
+{
+    let a = 2;
+    var x = 2;
+    console.log("a in block: ", a);
+    console.log("x in block: ", x);
+    
+}
+
+console.log(x, a)
---
+++ 03_js_basics/01_variables_and_datatypes/02_datatypes.js
@@ -0,0 +1,23 @@
+// Data-types in JS: 
+//      1. Primitive Datatypes: Number, Null, Boolean, BigInt, String, Symbol, undefined
+//      2. Object
+
+let a = 10
+let b = null
+let c = true
+let d = 23098409283094820934n
+let e = "I am a string"
+let f = Symbol('10')
+let g = undefined
+
+console.log(a, typeof a)
+console.log(b, typeof b)
+console.log(c, typeof c)
+console.log(d, typeof d)
+console.log(e, typeof e)
+console.log(f, typeof f)
+console.log(g, typeof g)
+
+
+let str = '30'
+console.log(str-7);
