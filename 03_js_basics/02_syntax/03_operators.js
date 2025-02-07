bf2d6fa866d4595d25be5af2a24e870fec773917
deependra-parmar <dparmar@qodeleaf.com>
2025-02-07 17:38:16 +0530

    Operators

---
+++ 03_js_basics/02_syntax/03_operators.js
@@ -0,0 +1,7 @@
+// used to perform different operations 
+
+// Arithmetic operators: +,-,*,/,%,**,++,--
+// Assignment operators: =, +=, -=, *=, /=, %=
+// Comparison operators: >, <, >=, <=, !=, ===, !==
+// Logical Operators: &&, ||, !
+// Bitwise operators; &, |, ~, >>, <<
