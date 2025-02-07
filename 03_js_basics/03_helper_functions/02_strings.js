34fc87a5bc96cadb878d323ade6986cf5fcbdfda
deependra-parmar <dparmar@qodeleaf.com>
2025-02-07 18:07:33 +0530

    String methods

---
+++ 03_js_basics/03_helper_functions/02_strings.js
@@ -0,0 +1,13 @@
+// length property: getting the length of the string
+// toUpperCase(str): converts the whole string to uppercase
+// toLowerCase(str): converts the whole string to lowercase
+// indexOf(index): finds the first occurence of the substring in the string
+// lastIndexOf(index): finds the last occurence of the substring in the string
+// slice(start,end): used to extract a substring from start to end index
+// substring(start,end): used to extract a substring but if you give it -ve start, it will be 0
+// substr(start,characters): extract number of chars from start
+// charAt(index): finds the character at specific index
+// concat(string1, string2, ..., string n): concat all the strings
+// trim(): removes whitespaces from both the ends
+// replace(old_str, new_str): replace a substring with new one
+// split(delimiter): used to split the string with delimiter into an array
