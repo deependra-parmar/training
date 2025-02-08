865f270c0d5b7d5a217a4a29884c2bf979a12ab5
deependra-parmar <dparmar@qodeleaf.com>
2025-02-08 09:45:48 +0530

    Updated

--- 03_js_basics/01_variables_and_datatypes/02_datatypes.js
+++ 03_js_basics/01_variables_and_datatypes/02_datatypes.js
@@ -1,6 +1,9 @@
 // Data-types in JS: 
 //      1. Primitive Datatypes: Number, Null, Boolean, BigInt, String, Symbol, undefined
-//      2. Object
+//      2. Non-Primitives: Functions, Arrays and objects
+
+// Primitives are stored in the memory in the form of value, are not objects and doesn't have methods.
+// Non-primitives are stored in the form of reference, are mutable, has methods and are objects.
 
 let a = 10
 let b = null
