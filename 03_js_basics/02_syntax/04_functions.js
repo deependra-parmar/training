5663aba1bdd3ac6ebb4ae9e93739a4b680fa1583
deependra-parmar <dparmar@qodeleaf.com>
2025-02-10 12:12:09 +0530

    Types of functions

--- 03_js_basics/02_syntax/04_functions.js
+++ 03_js_basics/02_syntax/04_functions.js
@@ -7,7 +7,6 @@ function square(n){
 console.log("Square of 8 is: ", square(8));
 
 // Functions are eligible to change the properties and values of the original copy and the changes are visible globally outside the function as well.
-
 // Functions can be nested
 
 function addSquares(a,b){
@@ -34,3 +33,36 @@ const sayHello = () => {
 }
 
 sayHello();
+
+// Types of functions: 
+/*
+1. Regular functions: defined using function keyword followed by name, parameters, and block of code. 
+
+    function sum(a,b){
+        return a+b;
+    }
+
+2. Anonymous function: doesn't have a name associated. Return a value after in a variable. This is for single-use
+
+    let square = function(n){
+        return n*n;
+    }
+
+3. Immediately Invoked Function Expression(IIFE): runs as soon as browser finds it
+
+    (function(a, b){
+        return a+b;
+    })(2,4);
+
+4. Arrow functions: Concise way of writing function expressions. 
+
+    const greet = (name) => {
+        console.log(`Hello ${name}, Good Morning !`);
+    }
+
+5. Async functions: Functions that are made async for handling promises and asynchronous operations. 
+
+    async function(){
+        // async code here: fetch, setTimeOut, etc.
+    }
+*/
