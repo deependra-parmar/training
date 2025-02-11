84ab21aff2c188e33d374a98469b75cb49287fa5
deependra-parmar <dparmar@qodeleaf.com>
2025-02-11 11:45:40 +0530

    Closures: explored

--- 03_js_basics/05_revise/07_closures.js
+++ 03_js_basics/05_revise/07_closures.js
@@ -1,5 +1,6 @@
 // Closure is the combination of the lexical environment with the function so that you can associate data with the function that you want to work upon.
 // A closure is a feature in JS where an inner function has access to outer function's variables called as - a scope chain
+// Closures allow the functions to retain the information and variables even after the scope has finished execution.
 
 /*
     A closure has three scope chains: 
@@ -7,6 +8,8 @@
         1. outer function's variables
         2. its own variables
         3. global variables
+
+    During the execution, the parent function passes its own scope chain to child function called legacy and due to this, the inner function or child function can access the preserved legacy variables.
 */
 
 function outer(){
