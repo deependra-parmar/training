6e67725bb49a2384221f114518d323970288231f
deependra-parmar <dparmar@qodeleaf.com>
2025-02-18 13:09:51 +0530

    Flags in Regex

---
+++ 12_regex_and_callbacks/05_flags.js
@@ -0,0 +1,7 @@
+/*
+    Most commonly used Flags in Regex:
+        g : for global search and return/check all occurences
+        i : making the pattern searching case-insensitive
+        m : enables multi-line search (goes beyond \n too)
+        s : short for single line and also enables . to check for \n
+*/
