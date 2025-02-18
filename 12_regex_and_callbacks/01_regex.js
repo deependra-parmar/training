6a6a3970b4fb4fea4f7f841729defd0575e2700f
deependra-parmar <dparmar@qodeleaf.com>
2025-02-18 11:23:07 +0530

    Regex: Introduction

---
+++ 12_regex_and_callbacks/01_regex.js
@@ -0,0 +1,6 @@
+/*
+Regular expressions provide a powerful way to perform pattern matching on certain characters within strings of text. They offer a concise syntax to carry out complex tasks that otherwise would require lengthy code.
+
+Syntax of Regex: /pattern/flags --------> flags are optional
+
+*/
