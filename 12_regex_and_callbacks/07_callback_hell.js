db978bcb51d24281e330a21ff1242e7423baff21
deependra-parmar <dparmar@qodeleaf.com>
2025-02-18 17:54:05 +0530

    Regex and hof

--- 12_regex_and_callbacks/07_callback_hell.js
+++ 12_regex_and_callbacks/07_callback_hell.js
@@ -1,3 +1,12 @@
 /*
 Callback hell is basically the mess that is created when callbacks are subsequently passed to each other down the line one after another which ultimately results in lack of readability, maintainability and error handling.
+
+Higher order function is basically a function that either taken a function as an argument or returns a function. 
+
+function higherOrderFunction(callback){
+    // do specific task
+
+    callback();
+    return;
+}
 */
