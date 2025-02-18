81ee1d4eae7e85cdd7aa78c6dab5eb729b522127
deependra-parmar <dparmar@qodeleaf.com>
2025-02-19 05:19:48 +0530

    Question 04

--- 13_regex_callbacks_assignment/04_question.js
+++ 13_regex_callbacks_assignment/04_question.js
@@ -5,7 +5,13 @@ Write a regex function to distinguish and pick the values of email address, phon
 
 const string = `Lorem ipsum dolor 9221122108 sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. mytraining@deqode.com Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. +91-20200-21210 Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce (+91)-20200-21210 ut placerat mt@test.inc orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.`
 
-const emailValidator = /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/g
-const phoneNumberValidator = //
+const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+const phoneRegex = /(\+?\(?\d{1,4}\)?[\s\-]?)?\(?\d{1,4}\)?[\s\-]?\d{1,4}[\s\-]?\d{1,4}/g;
 
-console.log(string.match(emailValidator));
+
+const emails = string.match(emailRegex);
+const phones = string.match(phoneRegex);
+
+
+console.log("Valid emails are: ", emails);
+console.log("Valid phone numbers are: ", phones);
