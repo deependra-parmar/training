db1ae38e1ea099e2fee141d6538ae60fcaa83a8d
deependra-parmar <dparmar@qodeleaf.com>
2025-02-18 17:50:43 +0530

    Callback hell

---
+++ 12_regex_and_callbacks/07_callback_hell.js
@@ -0,0 +1,3 @@
+/*
+Callback hell is basically the mess that is created when callbacks are subsequently passed to each other down the line one after another which ultimately results in lack of readability, maintainability and error handling.
+*/
