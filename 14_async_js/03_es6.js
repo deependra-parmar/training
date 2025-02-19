b40957a7cc41e828068556e2a67c99a16f7aafc8
deependra-parmar <dparmar@qodeleaf.com>
2025-02-19 12:17:34 +0530

    ES6 major updates

---
+++ 14_async_js/03_es6.js
@@ -0,0 +1,15 @@
+/*
+    ES6 aka EcmaScript 6 was the 6th release of JS in 2015 that came up with significant change in features.
+    Major features of ES6 are as follows: 
+
+        1. Blocked-Scope Variables: Rise of let and const with var for variable declaration.
+        2. String Interpolation using Template Literals
+        3. Arrow functions
+        4. Object Destructuring
+        5. Promises
+        6. Spread Operator
+        7. Enhanced Object Literals
+        8. Module exports and imports for more cleaner syntax
+        9. Special for loops(for...in, for...of)
+        10. Default parameters
+*/
