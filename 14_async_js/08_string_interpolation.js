9f0701647ca240aa7a41ffd1f0e4bc5bd81c1b40
deependra-parmar <dparmar@qodeleaf.com>
2025-02-19 13:37:27 +0530

    String Interpolation

---
+++ 14_async_js/08_string_interpolation.js
@@ -0,0 +1,11 @@
+/*
+    Interpolation is the process of embedding expressions or variables directly within strings, creating dynamic and flexible string creation. JavaScript primarily achieves this through template literals, introduced in ES6.
+
+    In ES5, the same was acheived via string concatenation.
+
+*/
+
+const name = "Deependra Parmar"
+const email = "dp@gmail.com"
+
+console.log(`Hey there, I am ${name} and you can get in touch with me via ${email}`);
