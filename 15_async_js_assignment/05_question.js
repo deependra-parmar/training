7a5ecd87eb8d2c36fd308d0b6625ad624dad2ab4
deependra-parmar <dparmar@qodeleaf.com>
2025-02-20 16:38:35 +0530

    Updated files

--- 15_async_js_assignment/05_question.js
+++ 15_async_js_assignment/05_question.js
@@ -18,3 +18,4 @@ var nextItem = function() {
 };
 
 */
+
--- test.js
+++ test.js
@@ -12,7 +12,25 @@ obj.print();
 
 */
 
+/*
 let str = 'hello'
 console.log(str);
 str = 'hey'
 console.log(str);
+
+*/
+
+
+// Temporal Deadzone: Temporal deadzone is the certain moment or period in the execution of the program in which the variables are declared but cannot be access and thus result in Reference Error. It occurs when the variables are declared using let, const and class.
+// This doesn't occur with var as variables with var are hoisted.
+
+function takeNumber(num){
+    console.log(num);
+}
+
+// takeNumber(num);
+num2 = 30;
+takeNumber(num2); // Var is defined but prints undefined;
+
+let num1 = 10;
+var num2 = 20;
