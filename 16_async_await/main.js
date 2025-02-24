47fabc2174a6bccd8a29198ca9f5088be05d23e2
deependra-parmar <dparmar@qodeleaf.com>
2025-02-24 12:29:27 +0530

    Async await with try-catch

--- 16_async_await/main.js
+++ 16_async_await/main.js
@@ -1,3 +1,10 @@
 import sayHello from "./module.js";
 
-await sayHello(); // If any function is async, so you can call it with await in another file/module. We need not to call it in another async scope.
+try{
+    const data = await sayHello(); // If any function is async, so you can call it with await in another file/module. We need not to call it in another async scope.
+
+    console.log(data);
+}
+catch(error){
+    console.error("Error: ", error.message);
+}
--- 16_async_await/module.js
+++ 16_async_await/module.js
@@ -1,5 +1,22 @@
 export default async function(){
-    setTimeout(() => {
-        console.log("I say Hello after 4 seconds");
-    }, 4000);
+    return new Promise((resolve, reject) => {
+        const resolutionRatio = Math.random();
+
+        setTimeout(() => {
+            console.log("Return data after 2 seconds...");
+            
+            if(resolutionRatio >= 0.5){
+                resolve({
+                    success: true,
+                    message: "Operation done successfully"
+                });
+            }
+            else{
+                reject({
+                    success: false,
+                    message: "Error performing operation..."
+                });
+            }
+        }, 2000);
+    });
 }
